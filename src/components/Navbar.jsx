471a45618a2bcc8ecbb29955a9ba16c5ae71f21f
unknown <jorge.orozco@cetys.edu.mx>
2023-05-11 11:08:34 -0700

    Pokedex Bootstrap pre-deployment

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import { NavLink } from 'react-router-dom'
+
+// NavLink y Link sustituyen a <a href='...'...</a>, porque la etiqueta <a> recarga la página y rompe el SPA.
+
+const Navbar = () => {
+  return (
+    <nav className='navbar navbar-expand-sm navbar-light bg-warning d-flex justify-content-between px-4'>
+      <NavLink className='navbar-brand' to='#'>Pokédex</NavLink>
+      <ul className='navbar-nav mr-auto mt-2 mt-lg-0'>
+        <li className='nav-item px-2'>
+          <NavLink className='nav-link' to='/'>Home</NavLink>
+        </li>
+        <li className='nav-item px-2'>
+          <NavLink className='nav-link' to='/about'>Acerca de Pokémon</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ src/pages/Home.jsx
@@ -0,0 +1,55 @@
+import { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+const Home = () => {
+  const [pokemons, setPokemons] = useState([]) // Lista de Pokémons
+  const [searchTerm, setSearchTerm] = useState('') // Palabra a buscar
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=151')
+      .then(response => response.json())
+      .then(data => setPokemons(data.results))
+      .catch(error => console.error(error))
+  }, [])
+
+  const handleSearch = (event) => {
+    setSearchTerm(event.target.value)
+  }
+  const filteredPokemons = pokemons.filter(pokemon => {
+    return pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
+  return (
+    <div className='container'>
+      <h1>Home</h1>
+
+      <form className='form-inline my-2 w-100'>
+        <input
+          type='text'
+          className='form-control'
+          placeholder='Who is that Pokemon?'
+          id='search'
+          value={searchTerm}
+          onChange={handleSearch}
+        />
+      </form>
+
+      <div className='row'>
+        {
+            filteredPokemons.map(pokemon => (
+              <div className='col-sm-4 mb-4' key={pokemon.name}>
+                <div className='card'>
+                  <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/')[6]}.png`} className='card-img-top' alt={pokemon.name} />
+                  <div className='card-body'>
+                    <Link to={`/pokemon/${pokemon.url.split('/')[6]}`}>
+                      <h5 className='card-title'>{pokemon.name}</h5>
+                    </Link>
+                  </div>
+                </div>
+              </div>
+            ))
+        }
+      </div>
+    </div>
+  )
+}
+export default Home
